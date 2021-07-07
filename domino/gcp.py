fb84213eaaf88dfc23f5f80ed7ddfe932aae54f9
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-07-06 17:17:47 -0700

    Update yaml manifests and [WIP] implement gcp

--- domino/gcp.py
+++ domino/gcp.py
@@ -22,40 +22,40 @@ def get_commands(pool, cmd, env):
 def launch_pod(run_name: str, pool: str, cmd: str, env: str, image: str = None):
     # Load the base manifest for launching Pods
     config = yaml.load(
-        open("scratch/sabri/pods/default-pod.yaml"),
+        open("scratch/sabri/pods/dev-pod.yaml"),
         Loader=yaml.FullLoader,
     )
 
-    # Wipe out the GPU node selector
-    config["spec"]["nodeSelector"] = {}
-    # Specify the pool
-    config["spec"]["nodeSelector"]["cloud.google.com/gke-nodepool"] = f"{pool}"
-    # Request GPUs
+    # # Wipe out the GPU node selector
+    #config["spec"]["nodeSelector"] = {}
+    # # Specify the pool
+    #config["spec"]["nodeSelector"]["cloud.google.com/gke-nodepool"] = f"{pool}"
+    # # Request GPUs
     config["spec"]["containers"][0]["resources"] = {
-        "limits": {"nvidia.com/gpu": pool.split("-")[-1]},
-        "requests": {"nvidia.com/gpu": pool.split("-")[-1]},
+         "limits": {"nvidia.com/gpu": 1},
+         "requests": {"nvidia.com/gpu": 1},
     }
 
-    # Set the name of the Pod
+    # # Set the name of the Pod
     config["metadata"]["name"] = config["spec"]["containers"][0]["name"] = run_name
-    # Set the name of the image we want the Pod to run
+    # # Set the name of the image we want the Pod to run
     if image is not None:
         config["spec"]["containers"][0]["image"] = image
 
-    # Put in a bunch of startup get_commands
+    # # Put in a bunch of startup get_commands
     config["spec"]["containers"][0]["command"] = ["bash", "-c"]
-    config["spec"]["containers"][0]["args"] = [
-        " && ".join(get_commands(pool, cmd, env))
-    ]
+    #config["spec"]["containers"][0]["args"] = [
+    #    " && ".join(get_commands(pool, cmd, env))
+    #]
 
     # Store it
     yaml.dump(config, open("temp.yaml", "w"))
 
     # Log
     print(f"Run name: {run_name}")
-
+    
     # Launch the Pod
     subprocess.call("kubectl apply -f temp.yaml", shell=True)
 
     # Clean up
-    os.remove("temp.yaml")
+    #os.remove("temp.yaml")
--- scratch/sabri/06-09_test_gcp.py
+++ scratch/sabri/06-09_test_gcp.py
@@ -2,8 +2,8 @@ from domino.gcp import launch_pod
 
 if __name__ == "__main__":
     launch_pod(
-        "test",
-        pool="t4-1-train",
+        "test2",
+        pool="t4-1",
         cmd="python /home/sabri/code/domino/scratch/sabri/06-09_train.py",
         env="domino",
     )
