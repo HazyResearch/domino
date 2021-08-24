fb2047319e0c97351659087c93c3bcec172e3704
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-08-23 20:20:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ REQUIRED = [
     "pre-commit",
     "pytorch-lightning",
     "gradio<2.0.0",  # 1.7.7
+    "meerkat @ git+https://github.com/robustness-gym/meerkat@dev",
     "terra @ git+https://github.com/seyuboglu/terra",
     "pandas",
     "numpy>=1.18.0",
@@ -21,7 +22,6 @@ REQUIRED = [
     "torchvision>=0.9.0",
     "wandb",
     "ray[default]",
-    "torchxrayvision",
 ]
 
 setuptools.setup(
@@ -32,7 +32,7 @@ setuptools.setup(
     description="Research package for automated slice discovery ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seyuboglu/domino",
+    url="https://github.com/HazyResearch/domino",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
