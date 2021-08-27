f4c0360c89c9fd6e772739a85093720a15e6ede1
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-08-27 02:18:24 +0000

    Add notebook for constructing vg tasks

--- domino/data/visual_genome.py
+++ domino/data/visual_genome.py
@@ -3,6 +3,73 @@ from meerkat.contrib.visual_genome import read_visual_genome_dps
 DATASET_DIR = "/home/common/datasets/visual-genome"
 
 ATTRIBUTE_GROUPS = {
+    "darkness": ["dark", "bright"],
+    "dryness": ["wet", "dry"],
+    "colorful": ["colorful", "shiny"],
+    "leaf": ["leafy", "bare"],
+    "emotion": ["happy", "calm"],
+    "sports": ["baseball", "tennis"],
+    "flatness": ["flat", "curved"],
+    "lightness": ["light", "heavy"],
+    "gender": ["male", "female"],
+    "width": ["wide", "narrow"],
+    "depth": ["deep", "shallow"],
+    "hardness": ["hard", "soft"],
+    "cleanliness": ["clean", "dirty"],
+    "switch": ["on", "off"],
+    "thickness": ["thin", "thick"],
+    "openness": ["open", "closed"],
+    "height": ["tall", "short"],
+    "length": ["long", "short"],
+    "fullness": ["full", "empty"],
+    "age": ["young", "old", "new"],
+    "size": ["large", "small"],
+    "pattern": ["checkered", "striped", "dress", "dotted"],
+    "shape": ["round", "rectangular", "triangular", "square"],
+    "activity": [
+        "waiting",
+        "drinking",
+        "playing",
+        "eating",
+        "cooking",
+        "resting",
+        "sleeping",
+        "posing",
+        "talking",
+        "looking down",
+        "looking up",
+        "driving",
+        "reading",
+        "brushing teeth",
+        "flying",
+        "surfing",
+        "skiing",
+        "hanging",
+    ],
+    "pose": [
+        "walking",
+        "standing",
+        "lying",
+        "sitting",
+        "running",
+        "jumping",
+        "crouching",
+        "bending",
+        "grazing",
+    ],
+    "material": [
+        "wood",
+        "plastic",
+        "metal",
+        "glass",
+        "leather",
+        "leather",
+        "porcelain",
+        "concrete",
+        "paper",
+        "stone",
+        "brick",
+    ],
     "colors": [
         "white",
         "red",
@@ -28,7 +95,7 @@ ATTRIBUTE_GROUPS = {
         "maroon",
         "light blue",
         "light brown",
-    ]
+    ],
 }
 
 
@@ -49,7 +116,8 @@ def get_dps(dataset_dir: str = DATASET_DIR):
     dps = read_visual_genome_dps(dataset_dir)
     image_dp = dps["images"]
     attr_dp = dps["attributes"]
+    relationship_dp = dps["relationships"]
     object_dp = dps["images"].merge(dps["objects"], on="image_id")
 
     object_dp["object_image"] = object_dp.to_lambda(crop_object)
-    return image_dp, attr_dp, object_dp
+    return image_dp, attr_dp, object_dp, relationship_dp
