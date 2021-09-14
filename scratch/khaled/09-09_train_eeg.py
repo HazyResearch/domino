2b08f2c8c6220f92beb0212482ef49b1ba121d02
khaledsaab <ksaab@stanford.edu>
2021-09-14 08:57:50 -0700

    train slices runnning after pd update

--- scratch/khaled/09-09_train_eeg.py
+++ scratch/khaled/09-09_train_eeg.py
@@ -1,7 +1,8 @@
 import meerkat as mk
 import terra
-from meerkat.contrib.eeg import build_stanford_eeg_dp
 
+from domino.slices.eeg import build_slice
+from domino.utils import split_dp
 from domino.vision import train
 
 
@@ -17,24 +18,35 @@ def train_eeg(
         },
         dp=dp,
         input_column=input_column,
-        id_column="file_id",
+        id_column="id",
         target_column=target_column,
         ckpt_monitor="valid_auroc",
         batch_size=10,
         run_dir=run_dir,
         val_check_interval=10,
         num_workers=6,
-        valid_split="dev",
+        valid_split="valid",
         use_terra=True,
     )
 
 
 if __name__ == "__main__":
 
+    dp_splits_art = split_dp.out(412)
+    dp_age_art = build_slice(
+        "correlation",
+        409,
+        dp_splits_art,
+        correlate="age",
+        corr=0.9,
+        n=2500,
+        correlate_threshold=10,
+    )
+
     train_eeg(
-        dp=build_stanford_eeg_dp.out(run_id=211),
-        target_column="binary_sz",
-        input_column="eeg_input",
+        dp=dp_age_art,
+        target_column="target",
+        input_column="input",
     )
 
 # run_dir="/home/ksaab/Documents/domino/scratch/khaled/results",
