30b20af05cc401e228fbcab2f9e2066b4ae2420d
Maya Varma <maya1.varma@gmail.com>
2021-10-01 21:10:00 +0000

    more plotting code

--- domino/plot.py
+++ domino/plot.py
@@ -1,14 +1,23 @@
-PALETTE = ["#9CBDE8", "#53B7AE", "#EFAB79", "#E27E51", "#19416E", "#1B6C7B"]
+
+from domino.evaluate import run_sdms, run_sdm, score_sdm_explanations, score_sdms
+import pandas as pd
+import meerkat as mk
+
+#PALETTE = ["#9CBDE8", "#53B7AE", "#EFAB79", "#E27E51", "#19416E", "#1B6C7B"]
+PALETTE = ["#9CBDE8", "#316FAE", "#29B2A1", "#007C6E", "#FFA17A", "#A4588F"]
 
 def coherence_metric(grouped_df):
     return (grouped_df['auroc']>0.85) & (grouped_df['precision_at_10']>0.5)
 
 
 EMB_PALETTE = {
-    "random": "#19416E",
-    "bit": "#1B6C7B",
-    "clip": "#E27E51",
-    "mimic_multimodal": "#9CBDE8"
+    #"random": "#19416E",
+    "bit": PALETTE[0],
+    "imagenet": PALETTE[1],
+    "clip": PALETTE[2],
+    "mimic_multimodal": PALETTE[3],
+    "mimic_multimodal_class": PALETTE[4],
+    "mimic_imageonly": PALETTE[5],
 }
 
 def generate_group_df(run_sdms_id, score_sdms_id, slice_type):
@@ -25,8 +34,8 @@ def generate_group_df(run_sdms_id, score_sdms_id, slice_type):
     results_dp["emb_type"] = emb_col
 
     results_df = results_dp.to_pandas()
-    grouped_df = results_df.iloc[results_df.reset_index().groupby(["slice_name", "slice_idx", "sdm_class", "alpha", "emb_type"])['auroc'].idxmax().astype(int)]
-    grouped_df = grouped_df[grouped_df['emb_type'] != 'mimic_imageonly']
+    grouped_df = results_df.iloc[results_df.reset_index().groupby(["slice_name", "slice_idx", "sdm_class", "alpha", "emb_type"])['precision_at_10'].idxmax().astype(int)]
+    #grouped_df = grouped_df[grouped_df['emb_type'] != 'mimic_imageonly']
     grouped_df["alpha"] = grouped_df["alpha"].round(3)
 
     if(slice_type=="correlation"): grouped_df = grouped_df[grouped_df['alpha'] != 0.0]
