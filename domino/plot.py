b1fc88f90a2e7b27793081d162bb0472fc24b6cb
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-10-01 19:58:07 +0000

    Added sdm pallette

--- domino/plot.py
+++ domino/plot.py
@@ -1,1 +1,9 @@
-PALETTE = ["#9CBDE8", "#53B7AE", "#EFAB79", "#E27E51", "#19416E", "#1B6C7B"]
+EMB_PALETTE = {"random": "#19416E", "bit": "#1B6C7B", "clip": "#E27E51"}
+
+SDM_PALETTE = {
+    "domino.sdm.george.GeorgeSDM": "#9CBDE8",
+    "domino.sdm.multiaccuracy.MultiaccuracySDM": "#EFAB79",
+    "domino.sdm.spotlight.SpotlightSDM": "#1B6C7B",
+    "domino.sdm.confusion.ConfusionSDM": "#19416E",
+    "domino.sdm.gmm.MixtureModelSDM": "#53B7AE",
+}
