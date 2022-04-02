5bc49ecf9328f17067cc059973c68f595f88aa4f
Sabri Eyuboglu <eyuboglu@stanford.edu>
2022-04-02 21:12:26 +0000

    Update imagenet notebook

--- domino/__init__.py
+++ domino/__init__.py
@@ -1,6 +1,16 @@
 from ._embed import embed, encoders
 from ._slice.domino import DominoSlicer
 from ._slice.spotlight import SpotlightSlicer
+from ._describe.generate import generate_candidate_descriptions
+from ._describe import describe
 from .gui import explore
 
-__all__ = ["DominoSlicer", "SpotlightSlicer", "embed", "encoders", "explore"]
+__all__ = [
+    "DominoSlicer",
+    "SpotlightSlicer",
+    "embed",
+    "encoders",
+    "explore",
+    "describe",
+    "generate_candidate_descriptions",
+]
